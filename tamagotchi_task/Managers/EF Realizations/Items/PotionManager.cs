90d3c9bd11c3482a8ba79eec3e14ec206a384c9b
LittleFoxe <vlad.litvintsev@mail.ru>
2022-05-04 21:57:56 +0300

    Создал менеджеры предметов для инвентаря и магазина.

---
+++ tamagotchi_task/Managers/EF Realizations/Items/PotionManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tamagotchi_task.Domain;
+using tamagotchi_task.Managers.Interfaces.ItemInterfaces;
+
+namespace tamagotchi_task.Managers.EF_Realizations.Items
+{
+    public class PotionManager : IPotionManager
+    {
+        private readonly AppDbContext _db;
+
+        public PotionManager(AppDbContext context)
+        {
+            _db = context;
+        }
+
+        public IQueryable GetItems()
+        {
+            return _db.Potions;
+        }
+
+        public async Task DecreaseAmount(Guid itemCharacterID)
+        {
+            PotionCharacter pc = await _db.PotionCharacters.FirstOrDefaultAsync(c => c.Id == itemCharacterID);
+            if (pc != null) 
+            {
+                pc.Amount -= 1; //Убираем элемент при использовании
+                //Удаляем элемент таблицы, если эликсиров вообще не осталось
+                if (pc.Amount <= 0)
+                    _db.PotionCharacters.Remove(pc);
+
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Guid> FindItemCharacterAsync(Guid characterID, Guid itemID)
+        {
+            //Я старался следовать KISS. Честно.
+            //Этот код крайне не оптимален, но я пытался сделать его понятным
+            IQueryable<Character> characters = 
+                _db.Characters.Where(c => c.Id == characterID); //Ищем персонажей с таким ID
+            if(characters == null) //Если их нет, то нам делать нечего
+                return Guid.Empty; //То же самое, что и return null;
+
+            IQueryable<Potions> potions = 
+                _db.Potions.Where(d => d.Id == itemID); //Ищем нужные эликсиры
+
+            return await _db.PotionCharacters //Возвращаем элемент таблицы PotionCharacters
+                .Where(pc => pc.Characters == characters) //со всеми записями, имеющими нужного перса,
+                .Where(pc => pc.Potions == potions) //но с одной искомой записью, совпадающей по персу-эликсиру
+                .Select(pc => pc.Id) //Конвертируем список с класса PotionCharacter в Guid
+                .FirstOrDefaultAsync(); //Отбираем оттуда один элемент
+        }
+    }
+}
---
+++ tamagotchi_task/Managers/Interfaces/ItemInterfaces/IToyManager.cs
@@ -0,0 +1,31 @@
+﻿namespace tamagotchi_task.Managers.Interfaces.ItemInterfaces
+{
+    public interface IToyManager
+    {
+        /// <summary>
+        /// Возвращает коллекцию IQueryable из базы данных.
+        /// </summary>
+        public IQueryable GetItems();
+
+        /// <summary>
+        /// Декрементирует количество данного предмета в инвентаре,
+        /// если элемент таблицы ToyManager с таким ID был найден.
+        /// </summary>
+        /// <param name="itemCharacterID"></param>
+        /// <returns>
+        /// Декрементирует свойство Amount или удаляет элемент таблицы,
+        /// при его значении Amount меньше или равном 0.
+        /// </returns>
+        public Task DecreaseAmount(Guid itemCharacterID);
+
+        /// <summary>
+        /// Ищет элемент таблицы ToyManager по ID персонажа и ID предмета.
+        /// </summary>
+        /// <param name="characterID"></param>
+        /// <param name="itemID"></param>
+        /// <returns>
+        /// Возвращает ID элемента с совпадающими столбцами или "пустой" ID в обратном случае.
+        /// </returns>
+        public Task<Guid> FindItemCharacterAsync(Guid characterID, Guid itemID);
+    }
+}
