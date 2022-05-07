b6beb1bcdfcc5aee3fd4704d655fae5dbc380981
Aleksandr <savinyhal@gmail.com>
2022-05-08 00:11:09 +0300

    Разделил аватар в креейторе

---
+++ tamagotchi_task/Models/ViewModels/AvatarModel.cs
@@ -0,0 +1,8 @@
+namespace tamagotchi_task.Models.ViewModels;
+
+public class AvatarModel
+{
+    public string Color { get; set; }
+    public string Accessory { get; set; }
+    public string Wallpaper { get; set; }
+}
