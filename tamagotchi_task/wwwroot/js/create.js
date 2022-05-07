6b464562d1393107c477ff08fa02d3a2ce8e3d50
Aleksandr <savinyhal@gmail.com>
2022-05-07 16:35:36 +0300

    Добавлены поиск и фильтрация (вью)

--- tamagotchi_task/wwwroot/js/create.js
+++ tamagotchi_task/wwwroot/js/create.js
@@ -49,3 +49,20 @@ function showDate() {
 function hideDate() {
     document.getElementById('dateinput').style.display = "none";
 }
+
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
