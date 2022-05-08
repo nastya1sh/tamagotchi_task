5510b1089c5d81877b26f71a613b9df6fe35836d
LittleFoxe <vlad.litvintsev@mail.ru>
2022-05-08 23:47:24 +0300

    Отображение котейки.

---
+++ tamagotchi_task/Views/ViewComponents/ShowCharacterViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tamagotchi_task.Managers.Interfaces;
+
+namespace tamagotchi_task.Views.ViewComponents
+{
+    public class ShowCharacterViewComponent : ViewComponent
+    {
+        private readonly ICharacterManager _characterManager;
+
+        public ShowCharacterViewComponent(ICharacterManager characterManager)
+        {
+            _characterManager = characterManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var character = await _characterManager.FindCharacterByUser(User.Identity.Name);
+
+            return View(character);
+        }
+    }
+}
